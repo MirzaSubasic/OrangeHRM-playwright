0a4f7a1ce5a496eeb9c55d10a0b748d3a34c4044
Mirza Subasic <Mirza.Subasic@comtradeintegration.onmicrosoft.com>
2025-11-13 15:04:29 +0100

    leave page - add locators and method to fill required fields in form (incomplete)

--- pages/leave-page.ts
+++ pages/leave-page.ts
@@ -5,7 +5,11 @@ export class LeavePage extends BasePage {
     private assignLeaveButton: Locator;
     private selectEmployeeField: Locator;
     private selectEmployeeOption: Locator;
-    private logedInUserText: Locator
+    private logedInUserText: Locator;
+    private leaveTypeDropDown: Locator;
+    private fromDate: Locator;
+    private toDate: Locator;
+    private todayDateButton: Locator;
 
     constructor(page: Page){
         super();
@@ -13,6 +17,10 @@ export class LeavePage extends BasePage {
         this.selectEmployeeField = page.getByPlaceholder("Type for hints");
         this.selectEmployeeOption = page.getByRole("listbox").first();
         this.logedInUserText = page.locator('.oxd-userdropdown-name');
+        this.leaveTypeDropDown = page.locator('.oxd-select-text-input');
+        this.fromDate = page.getByPlaceholder("yyyy-dd-mm").first();
+        this.toDate = page.getByPlaceholder("yyyy-dd-mm").nth(1);
+        this.todayDateButton = page.getByRole('button', {name: 'Today'});
     }
 
     async goToAssignLeaveTab(){
@@ -26,13 +34,20 @@ export class LeavePage extends BasePage {
         await this.fillElement(this.selectEmployeeField, username, "entering employee name in field");
         await this.clickOnListedOptionsDiv(this.selectEmployeeOption, username, "Selected employee")
 
-        //leave type
         //date from
+        await this.clickElement(this.fromDate, "Clicked on from date field in leave form")
+        await this.clickElement(this.todayDateButton, "Clicked today button on calendar")
+
         //date to
+        await this.clickElement(this.toDate, "Clicked on to date field in leave form")
+        await this.clickElement(this.todayDateButton, "Clicked today button on calendar")
+        
+        //leave type
+        await this.selectDropdownByValue(this.leaveTypeDropDown, "US - Vacation", "Selecting leave type")
     }
 
     async getAvailableLeaveDays(){
-        
+
     }
 
     async submitLeave(){
